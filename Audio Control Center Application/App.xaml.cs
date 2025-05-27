bb699fe9c834bafe30754d94201a970bceba99e0
cnoble25 <56976647+cnoble25@users.noreply.github.com>
2025-05-27 13:56:59 -0400

    Created application
    Created the start of the application

---
+++ Audio Control Center Application/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Audio_Control_Center_Application
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Audio Control Center Application/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Audio_Control_Center_Application
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
