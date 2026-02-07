d64388b2ec2682ff73d57d4d7b5b15bedccebf65
cnoble25 <56976647+cnoble25@users.noreply.github.com>
2026-02-07 15:57:56 -0600

    added arduino file for fully setting this up
    dsad

---
+++ Arduino_File/Audio_Control_Center_Sliders/Audio_Control_Center_Sliders.ino
@@ -0,0 +1,53 @@
+#include<math.h>
+const int NUM_SLIDERS = 3;
+const int analogInputs[NUM_SLIDERS] = {A7,A6,A5};
+
+int analogSliderValues[NUM_SLIDERS];
+
+void setup() { 
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    pinMode(analogInputs[i], INPUT);
+  }
+
+  Serial.begin(31250);
+}
+
+void loop() {
+  updateSliderValues();
+  sendSliderValues(); // Actually send data (all the time)
+  // printSliderValues(); // For debug
+  delay(10);
+}
+
+void updateSliderValues() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+     analogSliderValues[i] = analogRead(analogInputs[i]);
+  }
+}
+
+void sendSliderValues() {
+  String builtString = String("");
+
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    builtString += String(analogSliderValues[i]);
+
+    if (i < NUM_SLIDERS - 1) {
+      builtString += String("|");
+    }
+  }
+  
+  Serial.println(builtString);
+}
+
+void printSliderValues() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    String printedString = String("Slider #") + String(i + 1) + String(": ") + String(analogSliderValues[i]) + String(" mV");
+    Serial.write(printedString.c_str());
+
+    if (i < NUM_SLIDERS - 1) {
+      Serial.write(" | ");
+    } else {
+      Serial.write("\n");
+    }
+  }
+}
